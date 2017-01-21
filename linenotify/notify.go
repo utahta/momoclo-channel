d83e655d69e84388ef0b388065644f716bd2ad88
utahta <labs.ninxit@gmail.com>
2017-01-21 18:06:13 +0900

    Add imageFile parameter to LINE Notify API

--- linenotify/notify.go
+++ linenotify/notify.go
@@ -1,18 +1,23 @@
 package linenotify
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"net/url"
+	"path"
 	"strings"
 
 	"github.com/pkg/errors"
 )
 
 type RequestNotify struct {
-	Client *http.Client
+	Client         *http.Client
+	imageBodyCache map[string][]byte
 }
 
 var (
@@ -20,24 +25,31 @@ var (
 )
 
 func NewRequestNotify() *RequestNotify {
-	return &RequestNotify{Client: http.DefaultClient}
+	return &RequestNotify{Client: http.DefaultClient, imageBodyCache: map[string][]byte{}}
 }
 
-func (r *RequestNotify) Notify(token, message, imageThumbnail, imageFullsize string) error {
-	v := url.Values{}
-	v.Add("message", message)
-	if imageThumbnail != "" {
-		v.Add("imageThumbnail", imageThumbnail)
-	}
-	if imageFullsize != "" {
-		v.Add("imageFullsize", imageFullsize)
+func (r *RequestNotify) Notify(token, message, imageThumbnail, imageFullsize, imageFile string) error {
+	var (
+		contentType string
+		body        io.Reader
+		err         error
+	)
+
+	if imageFile != "" {
+		if body, contentType, err = r.requestBodyWithImageFile(message, imageFile); err != nil {
+			return err
+		}
+	} else {
+		if body, contentType, err = r.requestBody(message, imageThumbnail, imageFullsize); err != nil {
+			return err
+		}
 	}
 
-	req, err := http.NewRequest("POST", "https://notify-api.line.me/api/notify", strings.NewReader(v.Encode()))
+	req, err := http.NewRequest("POST", "https://notify-api.line.me/api/notify", body)
 	if err != nil {
 		return err
 	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Type", contentType)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 
 	resp, err := r.Client.Do(req)
@@ -64,4 +76,53 @@ func (r *RequestNotify) Notify(token, message, imageThumbnail, imageFullsize str
 		return errors.New(root["message"].(string))
 	}
 	return nil
+}
+
+func (r *RequestNotify) requestBody(message, imageThumbnail, imageFullsize string) (io.Reader, string, error) {
+	v := url.Values{}
+	v.Add("message", message)
+	if imageThumbnail != "" {
+		v.Add("imageThumbnail", imageThumbnail)
+	}
+	if imageFullsize != "" {
+		v.Add("imageFullsize", imageFullsize)
+	}
+	return strings.NewReader(v.Encode()), "application/x-www-form-urlencoded", nil
+}
+
+func (r *RequestNotify) requestBodyWithImageFile(message, imageFile string) (io.Reader, string, error) {
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+
+	if err := w.WriteField("message", message); err != nil {
+		return nil, "", err
+	}
+
+	fw, err := w.CreateFormFile("imageFile", path.Base(imageFile))
+	if err != nil {
+		return nil, "", err
+	}
+
+	rb, ok := r.imageBodyCache[imageFile]
+	if ok {
+		if _, err := io.Copy(fw, bytes.NewBuffer(rb)); err != nil {
+			return nil, "", err
+		}
+	} else {
+		resp, err := r.Client.Get(imageFile)
+		if err != nil {
+			return nil, "", err
+		}
+		defer resp.Body.Close()
+		if _, err := io.Copy(fw, resp.Body); err != nil {
+			return nil, "", err
+		}
+		copy(r.imageBodyCache[imageFile], b.Bytes())
+	}
+
+	if err := w.Close(); err != nil {
+		return nil, "", err
+	}
+
+	return &b, w.FormDataContentType(), nil
 }

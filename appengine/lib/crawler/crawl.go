e5498e55537cffeb6949038b68248f0bdcc885e4
utahta <labs.ninxit@gmail.com>
2017-01-28 16:15:29 +0900

    Update time

--- appengine/lib/crawler/crawl.go
+++ appengine/lib/crawler/crawl.go
@@ -81,8 +81,8 @@ func crawlChannelClients(ctx context.Context) []*crawler.ChannelClient {
 	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
 	now := timeNow().In(jst)
 
-	// every week on Sunday, 16:55 <= now <= 19:00
-	if now.Weekday() == time.Sunday && ((now.Hour() == 16 && now.Minute() >= 55) || now.Hour() >= 17) && now.Hour() <= 19 {
+	// every week on Sunday, 16:55 <= now <= 17:59
+	if now.Weekday() == time.Sunday && ((now.Hour() == 16 && now.Minute() >= 55) || now.Hour() == 17) {
 		clients = append(clients, retrieveChannelClient(crawler.NewHappycloChannelClient(model.GetHappycloLatestEntryURL(ctx), option)))
 	}
 

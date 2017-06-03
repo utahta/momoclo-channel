e9166dd926031c717cebbe3739db09d47d251f8a
utahta <labs.ninxit@gmail.com>
2017-06-04 02:28:15 +0900

    Use errgroup package
    memory saving

--- appengine/lib/linenotify/notify.go
+++ appengine/lib/linenotify/notify.go
@@ -5,70 +5,114 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/pkg/errors"
-	"github.com/utahta/go-atomicbool"
 	"github.com/utahta/go-linenotify"
 	"github.com/utahta/momoclo-channel/appengine/lib/log"
 	"github.com/utahta/momoclo-channel/appengine/model"
 	"github.com/utahta/momoclo-crawler"
 	"golang.org/x/net/context"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/appengine/urlfetch"
 )
 
+const timeout = 540 * time.Second
+
 // Send message to LINE Notify
 func NotifyMessage(ctx context.Context, message string) error {
-	// [Notify Name] が付くので先頭に改行をいれて調整
-	return notifyMessage(ctx, fmt.Sprintf("\n%s", message), "")
-}
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
 
-// Send channel message and images to LINE Notify
-func NotifyChannel(ctx context.Context, ch *crawler.Channel) error {
-	errFlg := atomicbool.New(false)
-	var wg sync.WaitGroup
-	wg.Add(len(ch.Items))
-	for _, item := range ch.Items {
-		go func(ctx context.Context, item *crawler.ChannelItem) {
-			defer wg.Done()
-
-			if err := model.NewLineItem(item).Put(ctx); err != nil {
-				return
-			}
-			if err := notifyChannelItem(ctx, ch.Title, item); err != nil {
-				errFlg.Set(true)
-				log.GaeLog(ctx).Error(err)
-			}
-		}(ctx, item)
-	}
-	wg.Wait()
-
-	if errFlg.Enabled() {
-		return errors.New("Errors occured in linenotify.NotifyChannel")
-	}
-	return nil
-}
-
-func notifyMessage(ctx context.Context, message, imageURL string) error {
-	if disabled() {
-		return nil
-	}
-
-	glog := log.NewGaeLogger(ctx)
-
-	query := model.NewLineNotificationQuery(ctx)
-	items, err := query.GetAll()
+	c, err := newClient(ctx)
 	if err != nil {
 		return err
 	}
 
-	var errCount int32
-	reqCtx, cancel := context.WithTimeout(ctx, 540*time.Second)
+	// [Notify Name] が付くので先頭に改行をいれて調整
+	return c.notifyMessage(fmt.Sprintf("\n%s", message), "")
+}
+
+// Send channel message and images to LINE Notify
+func NotifyChannel(ctx context.Context, ch *crawler.Channel) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
-	c := linenotify.New()
-	c.HTTPClient = urlfetch.Client(reqCtx)
+
+	c, err := newClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	eg := &errgroup.Group{}
+	for _, item := range ch.Items {
+		item := item
+		eg.Go(func() error {
+			if err := model.NewLineItem(item).Put(ctx); err != nil {
+				return nil
+			}
+			if err := c.notifyChannelItem(ch.Title, item); err != nil {
+				log.GaeLog(ctx).Error(err)
+				return err
+			}
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		return errors.New("Errors occurred in NotifyChannel")
+	}
+	return nil
+}
+
+type client struct {
+	*linenotify.Client
+	users   []*model.LineNotification
+	context context.Context
+	log     log.Logger
+}
+
+func newClient(ctx context.Context) (*client, error) {
+	c := &client{
+		Client:  linenotify.New(),
+		context: ctx,
+		log:     log.NewGaeLogger(ctx),
+	}
+	c.HTTPClient.Transport = &urlfetch.Transport{Context: ctx}
+
+	query := model.NewLineNotificationQuery(ctx)
+	users, err := query.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	c.users = users
+
+	return c, nil
+}
+
+func (c *client) notifyChannelItem(title string, item *crawler.ChannelItem) error {
+	message := fmt.Sprintf("\n%s\n%s\n%s", title, item.Title, item.Url)
+
+	if len(item.Images) > 0 {
+		image := item.Images[0]
+		if err := c.notifyMessage(message, image.Url); err != nil {
+			return err
+		}
+
+		for _, image := range item.Images[1:] {
+			if err := c.notifyMessage(" ", image.Url); err != nil {
+				return err
+			}
+		}
+	} else {
+		return c.notifyMessage(message, "")
+	}
+	return nil
+}
+
+func (c *client) notifyMessage(message, imageURL string) error {
+	if disabled() {
+		return nil
+	}
 
 	// prepare cached image
 	if imageURL != "" {
@@ -80,21 +124,20 @@ func notifyMessage(ctx context.Context, message, imageURL string) error {
 	}
 
 	var workQueue = make(chan bool, 100) // max goroutine
-	var wg sync.WaitGroup
-	for _, item := range items {
+	eg := &errgroup.Group{}
+	for _, user := range c.users {
+		user := user
 		workQueue <- true
-		wg.Add(1)
-		go func(item *model.LineNotification) {
-			defer wg.Done()
+
+		eg.Go(func() error {
 			defer func() {
 				<-workQueue
 			}()
 
-			token, err := item.Token()
+			token, err := user.Token()
 			if err != nil {
-				glog.Error(err)
-				atomic.AddInt32(&errCount, 1)
-				return
+				c.log.Errorf("Failed to get token. hash:%v err:%v", user.Id, err)
+				return err
 			}
 
 			var image io.Reader
@@ -102,40 +145,22 @@ func notifyMessage(ctx context.Context, message, imageURL string) error {
 				image = bytes.NewReader(b)
 			}
 
-			if err := c.Notify(token, message, "", "", image); err != nil {
-				if err == linenotify.ErrNotifyInvalidAccessToken {
-					item.Delete(ctx)
-					glog.Infof("Delete LINE Notify token. hash:%s", item.Id)
-				} else {
-					glog.Error(err)
-				}
-				atomic.AddInt32(&errCount, 1)
-			}
-		}(item)
-	}
-	wg.Wait()
-
-	glog.Infof("LINE Notify. message:%s imageURL:%s len:%d errCount:%d", message, imageURL, len(items), errCount)
-	return nil
-}
-
-func notifyChannelItem(ctx context.Context, title string, item *crawler.ChannelItem) error {
-	message := fmt.Sprintf("\n%s\n%s\n%s", title, item.Title, item.Url)
-
-	if len(item.Images) > 0 {
-		image := item.Images[0]
-		if err := notifyMessage(ctx, message, image.Url); err != nil {
-			return err
-		}
-
-		for _, image := range item.Images[1:] {
-			if err := notifyMessage(ctx, " ", image.Url); err != nil {
+			err = c.Notify(token, message, "", "", image)
+			if err == linenotify.ErrNotifyInvalidAccessToken {
+				user.Delete(c.context)
+				c.log.Infof("Delete LINE Notify token. hash:%s", user.Id)
+			} else if err != nil {
+				c.log.Error("Failed to notify. hash:%v err:%v", user.Id, err)
 				return err
 			}
-		}
-	} else {
-		return notifyMessage(ctx, message, "")
+			return nil
+		})
 	}
+
+	if err := eg.Wait(); err != nil {
+		c.log.Warningf("Errors occurred in notifyMessage.")
+	}
+	c.log.Infof("LINE Notify. message:%s imageURL:%s len:%d", message, imageURL, len(c.users))
 	return nil
 }
 

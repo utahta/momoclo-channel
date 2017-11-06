8a26c053758a984da7a3d46dd745315ec157d401
utahta <labs.ninxit@gmail.com>
2017-11-07 00:43:46 +0900

    Revert LatestEntryRepository interface

--- domain/entity/latest_entry.go
+++ domain/entity/latest_entry.go
@@ -19,12 +19,24 @@ const (
 )
 
 type (
+	// LatestEntry for confirm last updated entry url
 	LatestEntry struct {
 		ID        string `datastore:"-" goon:"id"`
 		Code      string
 		URL       string
 		CreatedAt time.Time
 		UpdatedAt time.Time
+	}
+
+	// LatestEntryRepository interface
+	LatestEntryRepository interface {
+		Save(*LatestEntry) error
+		GetTamaiURL() string
+		GetMomotaURL() string
+		GetAriyasuURL() string
+		GetSasakiURL() string
+		GetTakagiURL() string
+		GetHappycloURL() string
 	}
 )
 

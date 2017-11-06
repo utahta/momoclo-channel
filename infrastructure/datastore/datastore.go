8fe1ba5a03841b9dd65e39028096c0712c517c4a
utahta <labs.ninxit@gmail.com>
2017-11-07 00:34:55 +0900

    datastore handler to private

--- infrastructure/datastore/datastore.go
+++ infrastructure/datastore/datastore.go
@@ -9,32 +9,32 @@ import (
 	"google.golang.org/appengine/datastore"
 )
 
-// Handler implements datastoreHandler interface using goon
-type Handler struct {
+// handler implements DatastoreHandler interface using goon
+type handler struct {
 	*goon.Goon
 }
 
-// New returns datastore Handler
+// New returns DatastoreHandler
 func New(c context.Context) persistence.DatastoreHandler {
-	return &Handler{
+	return &handler{
 		goon.FromContext(c),
 	}
 }
 
 // Put wraps goon.Put()
-func (h *Handler) Put(src interface{}) error {
+func (h *handler) Put(src interface{}) error {
 	_, err := h.Goon.Put(src)
 	return err
 }
 
 // PutMulti wraps goon.PutMulti()
-func (h *Handler) PutMulti(src interface{}) error {
+func (h *handler) PutMulti(src interface{}) error {
 	_, err := h.Goon.PutMulti(src)
 	return err
 }
 
 // Get wraps goon.Get()
-func (h *Handler) Get(dst interface{}) error {
+func (h *handler) Get(dst interface{}) error {
 	err := h.Goon.Get(dst)
 	if err == datastore.ErrNoSuchEntity {
 		return domain.ErrNoSuchEntity
@@ -43,18 +43,18 @@ func (h *Handler) Get(dst interface{}) error {
 }
 
 // GetMulti wraps goon.GetMulti()
-func (h *Handler) GetMulti(dst interface{}) error {
+func (h *handler) GetMulti(dst interface{}) error {
 	return h.Goon.GetMulti(dst)
 }
 
 // RunInTransaction wraps goon.RunInTransaction()
-func (h *Handler) RunInTransaction(fn func(h persistence.DatastoreHandler) error, o *persistence.TransactionOptions) error {
+func (h *handler) RunInTransaction(fn func(h persistence.DatastoreHandler) error, o *persistence.TransactionOptions) error {
 	var opts *datastore.TransactionOptions
 	if o != nil {
 		opts = &datastore.TransactionOptions{XG: o.XG, Attempts: o.Attempts}
 	}
 
 	return h.Goon.RunInTransaction(func(g *goon.Goon) error {
-		return fn(&Handler{g})
+		return fn(&handler{g})
 	}, opts)
 }
